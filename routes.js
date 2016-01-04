c820d0f2f1708c8e10115808ef734000fb02b57c
Alan Plotko <alan.plotko@gmail.com>
2016-01-03 20:45:33 -0500

    Set up passport and refactor routes
    Routes have been reorganized and now support passport. Only a local
    strategy is implemented, but this will help later on when implementing
    the services that Dash will support. Passport was the remaining piece to
    implement after MongoDB and the user model was set up. The user could be
    created and modified, but no persistent session was kept of that user
    upon logging in. Passport completes the authentication process.
    
    The following have been tested and are working:
    
    - User login
    - User registration
    - Default display name created from email address (the portion before
      the '@' symbol)
    - Account 2-hour lock upon 5 failed login attempts
    - Form error messages using the connect-flash package
    - Passport's handling of a persistent session for the user
    - Check if user is logged in before visiting pages that require the user
      to be logged in
    - Logout
    
    The following will be implemented in the future:
    
    - 'Forgot your password'
    - Email notification for account lock
    - Email verification
    - Display name changes (through a user settings page)

--- routes.js
+++ routes.js
@@ -1,10 +1,13 @@
-module.exports = function(app, debug) {
+// --------- Dependencies ---------
+var path = require('path');
+var User = require('./models/user');
+var validator = require('validator');
+var xss = require('xss');
+var debug = (process.env.NODE_ENV == 'dev');
 
-    var path = require('path');
-    var User = require('./user-model');
-    var validator = require('validator');
-    var xss = require('xss');
+module.exports = function(app, passport) {
 
+    // Define custom validation function for a password
     validator.extend('isValidPassword', function (str) {
         return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d!@#$%\^&*\"\>\<\ \'\~\`\:\;\?\/\\\\{\}\[\]\|\,\.)(+=._-]{8,}$/.test(str);
     });
@@ -36,100 +39,91 @@ module.exports = function(app, debug) {
      *  Routes for App Pages
     =============================*/
 
+    // --------- Front Page ---------
+
     app.get('/', function (req, res) {
-        res.render('index', {
-            heading: app.locals.defaultHeading
-        });
-    });
-
-    app.get('/dashboard', function (req, res) {
-        // To Do: protect pages requiring login
-        res.render('index', {
-            heading: 'Welcome back!'
-        });
-    });
-
-    app.post('/login', function(req, res) {
-        // Attempt to authenticate user
-        var email = validator.trim(req.body.email);
-        var pass = req.body.password;
-
-        User.getAuthenticated(email, pass, function(err, user, reason) {
-            if (err) throw err;
-
-            // Login succeeded
-            if (user) {
-                res.send({
-                    isError: false,
-                    message: 'Logging you in, ' + xss(user.displayName) + '...',
-                    location: '/dashboard'
-                });
-            }
-
-            // Login failed
-            var reasons = User.failedLogin;
-
-            if (debug) var logFlag = false;
-            switch (reason) {
-                case reasons.NOT_FOUND:
-                    if (debug) { console.log('DEBUG: NOT_FOUND'); logFlag = true; }
-                case reasons.PASSWORD_INCORRECT:
-                    if (debug && !logFlag) console.log('DEBUG: PASSWORD_INCORRECT');
-                    res.send({ 
-                        isError: true,
-                        message: 'Error: The email address or password is incorrect.'
-                    });
-                    break;
-                case reasons.MAX_ATTEMPTS:
-                    if (debug) console.log('DEBUG: MAX_ATTEMPTS');
-                    res.send({ 
-                        isError: true,
-                        message: 'Error: The account is temporarily locked.'
-                    });
-                    // To Do: Send email about account being locked
-                    break;
-            }
-        });
-    });
-
-    app.post('/register', function(req, res) {
-        // Clean and verify form input
-        var email = validator.trim(req.body.email);
-        var displayName = email.split('@')[0];
-        var pass = req.body.password;
-
-        if (validator.isEmail(email) && validator.isValidPassword(pass))
+        if (req.isAuthenticated())
         {
-            var newUser = new User({
-                email: email,
-                displayName: displayName,
-                password: pass
-            });
-
-            newUser.save(function(err) {
-                if (err)
-                {
-                    res.send({ 
-                        isError: true,
-                        message: err.toString()
-                    });
-                }
-                else
-                {
-                    res.send({ 
-                        isError: false,
-                        displayName: xss(displayName)
-                    });
-                }
-            });
+            return res.redirect('/dashboard');
         }
         else
         {
-            res.send({ 
-                isError: true,
-                message: 'Email address or password did not meet criteria. Please refresh the page and try again.'
+            res.render('index');
+        }
+    });
+
+    // --------- User Dashboard ---------
+
+    app.get('/dashboard', isLoggedIn, function(req, res) {
+        console.log(req.user);
+        res.render('dashboard', {
+            username: xss(req.user.displayName)
+        });
+    });
+
+    // --------- Dash Login ---------
+
+    app.get('/login', function(req, res) {
+        if (req.isAuthenticated())
+        {
+            return res.redirect('/dashboard');
+        }
+        else
+        {
+            res.render('login', {
+                message: req.flash('loginMessage')
             });
         }
     });
 
-}
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/dashboard',
+        failureRedirect : '/login',
+        failureFlash : true
+    }));
+
+    // --------- Dash Registration ---------
+
+    app.get('/register', function(req, res) {
+        res.render('register', { message: req.flash('registerMessage') });
+    });
+
+    app.post('/register', passport.authenticate('local-register', {
+        successRedirect : '/dashboard',
+        failureRedirect : '/register',
+        failureFlash : true
+    }));
+
+
+    // --------- Miscellaneous Routes & Helper Functions ---------
+
+    // Route middleware to ensure user is logged in
+    function isLoggedIn(req, res, next) {
+        // Proceed if user is authenticated
+        if (req.isAuthenticated())
+            return next();
+        // Otherwise, redirect to front page
+        res.redirect('/');
+    }
+
+    // Clear credentials and destroy session upon logout
+    app.get('/logout', function(req, res) {
+        req.logout();
+        req.session.destroy(function (err) {
+            res.redirect('/');
+        });
+    });
+
+    /*================================================================
+     *  If the route does not exist (error 404), go to the error 
+     *  page. This route must remain as the last defined route, so
+     *  that other routes are not overridden!
+    ==================================================================*/
+    app.all('*', function(req, res, next) {
+        var err = new Error();
+        err.status = 404;
+        err.message = 'Page Not Found';
+        err.description = 'That\'s strange... we couldn\'t find what you were looking for.<br /><br />If you\'re sure that you\'re in the right place, let the team know.<br /><br />Otherwise, if you\'re lost, you can find your way back to the front page using the button below.';
+        next(err);
+    });
+};
