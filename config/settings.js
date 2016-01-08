0815f8935465a54cd23258bca221639465be2554
Alan Plotko <alan.plotko@gmail.com>
2016-01-08 12:59:20 -0500

    Add new dev settings
    - Add url for the dev environment
    - Add connection settings for Facebook

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,14 @@
 // Define variables for development and production environments
 module.exports.settings = {
+    'connections': {
+        'facebook': {
+            'clientID': process.env.DASH_FACEBOOK_APP_ID,
+            'clientSecret': process.env.DASH_FACEBOOK_APP_SECRET
+        }
+    },
     'dev': {
-        'MONGO_URI': 'mongodb://dashbot:dash@127.0.0.1:27017/dash'
+        'MONGO_URI': 'mongodb://dashbot:dash@127.0.0.1:27017/dash',
+        'url': 'http://localhost:3000'
     },
     'prod': {
         'MONGO_URI': process.env.DASH_MONGODB_URL
