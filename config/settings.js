8ec7922c601d0f0ae48d9a36810798679ae1011a
Alan Plotko <alan.plotko@gmail.com>
2016-07-23 22:03:45 -0400

    Update add user command; delete prod env settings

--- config/settings.js
+++ config/settings.js
@@ -16,51 +16,8 @@ var settings = {
     }
   },
   DEV: {
-    MONGO_URI: 'mongodb://dashbot:dash@127.0.0.1:27017/dash',
+    MONGO_URI: 'mongodb://dashbot:dash@127.0.0.1:27017/dash_test',
     URL: 'http://localhost:3000',
-    EMAIL_SETTINGS: {
-      HOST: process.env.DASH_EMAIL_HOST,
-      PORT: process.env.DASH_EMAIL_PORT,
-      AUTH: {
-        USER: process.env.DASH_EMAIL_USER,
-        PASS: process.env.DASH_EMAIL_PASS
-      },
-      SECURE: true
-    },
-    VERIFY_EMAIL_FORMAT: {
-      FROM:
-        process.env.DASH_EMAIL_NAME + ' <' +
-        process.env.DASH_EMAIL_USER + '>',
-      SUBJECT:
-        'Pending: Dash Account Verification',
-      HTML:
-        '<div style="text-align: center;"><h1>Dash</h1><h2>' +
-        '<i>Account Verification</i></h2></div><p>Click the ' +
-        'following link to confirm your account:</p>' +
-        '<p>${URL}</p><p>- DashBot</p>',
-      TEXT:
-        'Dash Account Verification: Please confirm your ' +
-        'account by clicking the following link: ${URL} - DashBot'
-    },
-    CONFIRM_EMAIL_FORMAT: {
-      FROM:
-        process.env.DASH_EMAIL_NAME + ' <' +
-        process.env.DASH_EMAIL_USER + '>',
-      SUBJECT:
-        'Dash Account Verified!',
-      HTML:
-        '<div style="text-align: center;"><h1>Dash</h1><h2>' +
-        '<i>Account Verification</i></h2></div><p>Your Dash ' +
-        'account has been successfully verified. Welcome to Dash!</p>' +
-        '<p>- DashBot</p>',
-      TEXT:
-        'Dash Account Verification: Your account has been ' +
-        'successfully verified. Welcome to Dash! - DashBot'
-    }
-  },
-  PROD: {
-    MONGO_URI: process.env.DASH_MONGODB_URL,
-    URL: 'http://localhost:3000', // Default to localhost for now
     EMAIL_SETTINGS: {
       HOST: process.env.DASH_EMAIL_HOST,
       PORT: process.env.DASH_EMAIL_PORT,
